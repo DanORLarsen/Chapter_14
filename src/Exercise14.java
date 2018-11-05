951bb035a7f8de9e63abff4658c7f173c5f0b5c4
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-11-05 21:45:44 +0100

    Added comments in code.

--- src/Exercise14.java
+++ src/Exercise14.java
@@ -9,25 +9,25 @@ public class Exercise14 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        GridPane pane = new GridPane();
-        int i = 0;
+        GridPane pane = new GridPane(); //Using GridPane since it got an optimal layout for Checkers
+        int i = 0;//Using i to decide if it should be Black (default) or white (changed)
         for (int r = 0; r < 8; r++) {
-            i++;
+            i++; //So its doesn't start with the same color in each row/column
             for (int c = 0; c < 8; c++) {
-                Rectangle r1 = new Rectangle(25,25,25,25);
+                Rectangle r1 = new Rectangle(25,25,25,25);//Size 25, and placed 25 and 25 away from top and side (prev rectangle aswell)
                 if (i % 2 == 0)
-                {r1.setFill(Color.WHITE);}
-                pane.add(r1, c, r);
+                {r1.setFill(Color.WHITE);} //BECOME WHITE
+                pane.add(r1, c, r); //Add by row and column
                 i++;
             }
         }
         Scene scene = new Scene(pane);
         primaryStage.setTitle("Exercise 14");
         primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.show();//CREATE SHOW AND EVERYTHING
     }
 
     public static void main(String[] args) {
-        launch();
+        launch();//LAUNCH IT!
     }
 }
